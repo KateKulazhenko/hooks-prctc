61a2edfca26769366443b9160569ca2cd4615003
Katsiaryna <>
2020-03-26 21:54:55 +0300

    Created project

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react';
+import { Search } from '../components/Search';
+import { Card } from '../components/Card';
+
+export const Home = () => {
+    const cards = new Array(15).fill('').map((_, index) => index)
+    return (
+        <Fragment>
+            <Search />
+            <div className="row">
+                {cards.map((card) => {
+                    return (
+                        <div className="col-sm-4 mb-4">
+                            <Card />
+                        </div>
+                    );
+                })}
+            </div>
+        </Fragment>
+    );
+};
