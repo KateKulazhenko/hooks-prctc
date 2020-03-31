e7745416b5e927da50d2a9c1b7d7bb7d10b466e3
Katsiaryna <>
2020-03-31 11:26:17 +0300

    Created context

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { AlertContext } from '../context/alert/alertContex';
 
 export const Search = () => {
+    const [value, setValue] = useState('');
+    const { show } = useContext(AlertContext);
+    const onSubmit = event => {
+        if (event.key !== 'Enter') {
+            return
+        }
+
+        if (value.trim()) {
+            console.log('Make request');
+        } else {
+            show('Enter user name');
+        }
+    }
+
     return (
         <div className="form-group">
             <input 
                 type="text"
                 className="form-control"
                 placeholder="Enter username"
+                value={value}
+                onChange={event => setValue(event.target.value)}
+                onKeyPress={onSubmit}
             />
         </div>
     );
